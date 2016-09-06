ea64c54d2defb2fb63c9f40b8b788efd1c66a050
harish <hb.harishbalaji@gmail.com>
2016-09-06 15:38:03 -0400

    looper

--- src/Wloop.java
+++ src/Wloop.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 public class Wloop {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		int secretNumber, guess = 0;//guess is initialized to 0
+		int secretNumber,secretNumber2, guess = 0;//guess is initialized to 0
 		
 		secretNumber = 5;
+		secretNumber2 = 123;
 		
 		System.out.println("I'm thinking of a number between 1 and 1000");
 		System.out.print("Enter the number:");
 		guess = keyboard.nextInt();
 		
-		while ( guess != secretNumber )
+		while ( guess != secretNumber2 && guess != secretNumber)
 		{
 			System.out.println("\nYou are wrong. Try again.");
 			System.out.println("Enter the number: ");
